312018ebf289181c97396361ca6f50b8cceb33da
travisbennett03 <tbennett1996@yahoo.com>
2022-10-27 11:57:32 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a} / {b} is {div} remainder {mod}");
+
+            var r = double.Parse(Console.ReadLine());
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r);
+
+            Console.WriteLine($"The area of a circle with radius of {r} is {areaOfCircle}");
+         
         }
     }
 }
